d5d9c3563da24e7fb19d195aa47d3f169117247e
fnetti90 <f.netti@frankhood.it>
2022-06-30 09:49:35 +0200

    FRANK-911 update commands import csv

--- pepposaur_project/pokedex/management/commands/import_pokemon.py
+++ pepposaur_project/pokedex/management/commands/import_pokemon.py
@@ -9,44 +9,46 @@ class Command(BaseCommand):
     help = "Import Pokemon"
 
     def add_arguments(self, parser) -> None:
-        parser.add_argument("path_csv", type=str, help="Insert path of csv file.")
-        parser.add_argument("path_image", type=str, help="Insert the path of the folder containing the iamges.")
+        parser.add_argument("path_file_csv", type=str, help="Insert path of csv file.")
+        parser.add_argument("path_file_image", type=str, help="Insert the path of the folder containing the iamges.")
 
     def handle(self, *args, **options):
-        # ==========================
-        # Pokemon
-        # ==========================
-
-        path_csv = options['path_csv']
-        path_image = options['path_image']
-
-        file = open(f'{path_csv}/pokemon.csv', "r")
+        path_file_csv = options['path_file_csv']
+        path_file_image = options['path_file_image']
+        file = open(f'{path_file_csv}', "r")
         reader = csv.reader(file)
+        next(reader) # ignoro la prima riga
 
-        for row in reader:
+        for idx, row in enumerate(reader):
             id_image = row[0]
-            for _, _, files in os.walk(path_image):
+            for _, _, files in os.walk(path_file_image):
                 for file in files:
                     filename = file.split(".")[0]
                     if filename == id_image:
-                        with open(f"{path_image}/{file}", "rb") as f:
-                            pokemon = Pokemon.objects.create(
-                                id=row.index,
-                                name=row[1],
+                        with open(f"{path_file_csv}", "rb") as f:
+                            pokemon, created = Pokemon.objects.get_or_create(
+                                id=idx,
                                 slug=slugify(row[1]),
-                                # typology_one=slugify(row[2]),
-                                # typology_two=slugify(row[3]),
-                                total=row[4],
-                                health_points=row[5],
-                                attack=row[6],
-                                defense=row[7],
-                                special_attack=row[8],
-                                special_defense=row[9],
-                                speed=row[10],
-                                is_legendary=True if row[12] == "True" else False,
+                                defaults={
+                                    "name":row[1],
+                                    # typology_one:row[2],
+                                    # typology_two:row[3],
+                                    "total":row[4],
+                                    "health_points":row[5],
+                                    "attack":row[6],
+                                    "defense":row[7],
+                                    "special_attack":row[8],
+                                    "special_defense":row[9],
+                                    "speed":row[10],
+                                    "is_legendary":True if row[12] == "True" else False,
+                                    }
+                                
                             )
-                            # pokemon.image.save(content=f, name=f.name)
-                            pokemon.image = File(f, name=os.path.basename(f.name)) # mette il file in memoria in modo tale da poter salavere il file
-                            pokemon.save()
-                            print(f"Pokemon {pokemon.name} importato.")
+                            if created:
+                                # pokemon.image.save(content=f, name=f.name)
+                                pokemon.image = File(f, name=os.path.basename(f.name)) # mette il file in memoria in modo tale da poter salvare il file
+                                pokemon.save()
+                                print(f"Pokemon {pokemon.name} importato.")
+                            else:
+                                print(f"Pokemon {pokemon.name} già importato.")
 
